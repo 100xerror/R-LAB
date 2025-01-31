05768d2bc814fb5ece1e267f93a49996fae9e7fb
companion :) <akshcodez@gmail.com>
2025-01-31 10:57:20 +0530

    Update A2-Q5.R

--- A2-Q5.R
+++ A2-Q5.R
@@ -1,8 +1,19 @@
 v = c(2, 6, 9, 2)
 
+# mode ka code
+
 getmode = function(v) {
-  uniqv = unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
+  uniqv = unique(v) # v ki unique values ko ek naye vector me dal diya
+  uniqv[which.max(tabulate(match(v, uniqv)))] #tabulate->occurence ki indexing
 }
 val = getmode(v)
 print(val)
+
+# maximum value ka index return
+
+get_max_index = function(v){
+	return (which.max(v))
+}
+
+ans = get_max_index(v)
+print(ans)
