0f8305cf5c6d469cdae1b2e575c62d06d9950903
companion :) <akshcodez@gmail.com>
2025-02-14 10:21:02 +0530

    Update A3-Q2.R

--- A3-Q2.R
+++ A3-Q2.R
@@ -2,5 +2,5 @@ mean_test_score = 72
 std_dev = 15.2
 # find probability s.t. P(X>=84)
 # pnorm(q,mean,sd) calculates P(X<=q)
-final_prob = 100 - pnorm(84,mean_test_score,std_dev)
-print(final_prob)
+final_prob = pnorm(100,mean_test_score,std_dev) - pnorm(84,mean_test_score,std_dev)
+print(final_prob*100)
